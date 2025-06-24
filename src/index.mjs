224d79e946c1365522845970c0610dcf59ca0c36
Railley Nickolei Vince Bautisa <railleynickoleivincebautista@gmail.com>
2025-06-25 04:24:03 +0800

    validation Schema

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,12 @@
 import express from "express";
-import { query } from "express-validator";
-
+import {
+  query,
+  validationResult,
+  body,
+  matchedData,
+  checkSchema,
+} from "express-validator";
+import { checkUserValidationSchema } from "../utils/validationSchemas.mjs";
 const app = express();
 //convert body to json
 app.use(express.json());
@@ -44,43 +50,75 @@ app.get("/", (req, res) => {
   res.send("Home").status(200);
 });
 
-app.get("/api/users", (req, res) => {
-  // destrucutre query parameters
-  const {
-    query: { filter, value },
-  } = req;
-  // if filter and value is undefined
-  if (filter && value) {
-    return res.send(mockUsers.filter((user) => user[filter].includes(value)));
+app.get(
+  "/api/users",
+  query("filter")
+    .isString()
+    .notEmpty()
+    .withMessage("must not Empty")
+    .isLength({ min: 3, max: 10 })
+    .withMessage("must be 3 to 10 characters"),
+  (req, res) => {
+    const result = validationResult(req);
+    console.log(result);
+    // destrucutre query parameters
+    const {
+      query: { filter, value },
+    } = req;
+    // if filter and value is undefined
+    if (filter && value) {
+      return res.send(mockUsers.filter((user) => user[filter].includes(value)));
+    }
+    return res.send(mockUsers);
   }
-  return res.send(mockUsers);
-});
+);
 
 ////INSERT
-app.post("/api/users", (req, res) => {
-  const { body } = req;
+app.post("/api/users", checkSchema(checkUserValidationSchema), (req, res) => {
+  const result = validationResult(req); //checks if there is any validaiton errors
+  if (!result.isEmpty())
+    return res.status(400).send({ errors: result.array() });
+  const data = matchedData(req); // this will extract the validated fields from request
   const newUser = {
     id: mockUsers[mockUsers.length - 1].id + 1,
-    ...body,
+    ...data,
   };
   mockUsers.push(newUser);
-  return res.sendStatus(201).send(newUser);
+  return res.status(201).send(newUser);
 });
 ///UPDATE BUT IN BATCH
-app.put("/api/users/:id", resolveUserUserById, (req, res) => {
-  const { body, findUserIndex } = req;
-  mockUsers[findUserIndex] = {
-    id: mockUsers[findUserIndex].id,
-    ...body,
-  };
-  return res.send(mockUsers[findUserIndex]).status(200);
-});
+app.put(
+  "/api/users/:id",
+  checkSchema(checkUserValidationSchema),
+  resolveUserUserById,
+  (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty())
+      return res.status(400).send({ errors: result.array() });
+    const data = matchedData(req);
+    const { findUserIndex } = req;
+    mockUsers[findUserIndex] = {
+      id: mockUsers[findUserIndex].id,
+      ...data,
+    };
+    return res.send(mockUsers[findUserIndex]).status(200);
+  }
+);
 // UPDATE THE OBJECT BUT IN ONLY SPECIFIC OBJECT KEYS
-app.patch("/api/users/:id", resolveUserUserById, (req, res) => {
-  const { body, findUserIndex } = req;
-  mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
-  return res.send(mockUsers[findUserIndex]).status(200);
-});
+app.patch(
+  "/api/users/:id",
+  checkSchema(checkUserValidationSchema),
+  resolveUserUserById,
+  (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty())
+      return res.status(400).send({ errors: result.array() });
+    const data = matchedData(req);
+    const { findUserIndex } = req;
+    mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...data };
+    return res.send(mockUsers[findUserIndex]).status(200);
+  }
+);
 ///DELETE REQUEST
 app.delete("/api/users/:id", resolveUserUserById, (req, res) => {
   const { findUserIndex } = req;
---
+++ utils/validationSchemas.mjs
@@ -0,0 +1,23 @@
+export const checkUserValidationSchema = {
+  username: {
+    isLength: {
+      options: {
+        min: 5,
+        max: 32,
+      },
+      errorMessage:
+        "Must be minimun of 5 characters and maximum of 32 characters",
+    },
+    notEmpty: {
+      errorMessage: "Username cannot be empty",
+    },
+    isString: {
+      errorMessage: "Username must be String",
+    },
+  },
+  displayName:{
+    notEmpty:{
+      errorMessage: "Display name must not be empty"
+    }
+  }
+};
